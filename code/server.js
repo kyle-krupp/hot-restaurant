0b0ab2d143fd51b4b6a948ec5bb63a4758fa63be
HumanJBooF <humanjboof@gmail.com>
2018-09-10 19:37:44 -0400

    basic routes

--- code/server.js
+++ code/server.js
@@ -3,8 +3,32 @@ const parser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+let tables = [];
+let waitlast = [];
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/tables', (req, res) => {
+    res.sendFile(path.join(__dirname, 'tables.html'));
+});
+
+app.get('/reserve', (req, res) => {
+    res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/api/tables', () => {
+    return res.JSON(tables)
+});
+
+app.get('/api/waitlist', (req, res) => {
+    return res.JSON(waitlast)
+})
+
+app.get()
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`)
 });
